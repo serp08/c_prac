9e3708cf0df1ef5b95b02a7e17b28e89787340d8
serp <marcokhl2005@gmail.com>
2025-04-03 18:46:02 -0400

    3/6 bonus completed

--- bonus.c
+++ bonus.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Employee {
     char name[100];
@@ -9,22 +10,84 @@ struct Employee {
     float perf;
 };
 
-struct Employee addEmployee(){
+void addEmployee(struct Employee *list, int *numEmployees);
+void updateEmployee(struct Employee *list, int numEmployees);
+void displayEmployee(struct Employee list[], int numEmployees);
+void searchEmployeeByName(struct Employee list[], int numEmployees);
+struct Employee sortEmployeesByPerformance(struct Employee *out, struct Employee list[], int numEmployees);
+
+int main(){
+    int numEmployees = 0;
+    struct Employee *ppl = calloc(0, sizeof(struct Employee));
+    addEmployee(ppl, &numEmployees);
+    // updateEmployee(ppl, numEmployees);
+    addEmployee(ppl, &numEmployees);
+    struct Employee a[2];
+    sortEmployeesByPerformance((struct Employee*) a, ppl, numEmployees);
+    printf("%s", a[0].dept);
+    free(ppl);
+    return 0;
+}
+
+void addEmployee(struct Employee *list, int *numEmployees){
+    (*numEmployees)++;
     struct Employee e;
     printf("Enter employee name: ");
     scanf("%s", &e.name);
     printf("Enter employee ID: ");
     scanf("%d", &e.id);
     printf("Enter employee salary: ");
-    scanf("%.2f", &e.salary);
+    scanf("%f", &e.salary);
     printf("Enter employee department: ");
     scanf("%s", &e.dept);
     printf("Enter employee years of experience: ");
     scanf("%s", &e.exp);
-    e.perf = .3*e.exp + 0.7*e.salary;
-    return e;
+    e.perf = 0.3*e.exp + 0.7*e.salary;
+    list = (struct Employee *)realloc(list, sizeof(e) * (*numEmployees));
+    list[*numEmployees - 1] = e;
 }
 
-int main(){
-    return 0;
+void updateEmployee(struct Employee *list, int numEmployees){
+    int id;
+    printf("Enter Employee ID to update: ");
+    scanf("%d", &id);
+    for(int i = 0; i < numEmployees; i++){
+        if((*(list + i)).id == id){
+            printf("Enter new salary: ");
+            scanf("%f", &(*(list + i)).salary);
+            printf("Enter new department: ");
+            scanf("%s", &(*(list + i)).dept);
+            break;
+        }
+        if(i == numEmployees - 1){
+            printf("Employee not found.");
+        }
+    }
+}
+
+void displayEmployee(struct Employee list[], int numEmployees);
+
+void searchEmployeeByName(struct Employee list[], int numEmployees);
+
+struct Employee sortEmployeesByPerformance(struct Employee *out, struct Employee list[], int numEmployees){
+    int counter = 0;
+    while(counter <= numEmployees){
+        int highest = list[0].perf, idx = 0;
+        for(int i = 0; i < numEmployees; i++){
+            if(counter == 0){
+                if(list[i].perf > highest){
+                    highest = list[i].perf;
+                    idx = i;
+                }
+            }
+            else{
+                if(list[i].perf > highest && list[i].perf < (*(out + counter)).perf){
+                    highest = list[i].perf;
+                    idx = i;
+                }
+            }
+        }
+        *(out + counter) = list[idx];
+        counter++;
+    }
 }
